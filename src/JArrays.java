35a27f8e83b44deea74550256e5d2753072c64eb
Harsha Murupudi <itsmeeeharsha@gmail.com>
2023-11-07 16:01:02 -0600

    ADD: 2D arrays

--- src/JArrays.java
+++ src/JArrays.java
@@ -2,13 +2,13 @@ import java.util.Arrays;
 
 public class JArrays {
     //Variable Argument
-    public static void printText(String... textList){
-        for(String text: textList){
+    public static void printText(String... textList) {
+        for (String text : textList) {
             System.out.println(text);
         }
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         //Declaring
         int[] numbers = {1, 2, 3};
@@ -35,15 +35,25 @@ public class JArrays {
         Arrays.sort(sArray);
         // return index of the item if found or -1
         int rArray = Arrays.binarySearch(sArray, "Mark");
-        if(rArray >= 0 ){
+        if (rArray >= 0) {
             System.out.println("Found Mark in the list");
         }
 
         //Equality
-        int[] s1 = {1,2,3};
-        int[] s2 = {1,2,3};
+        int[] s1 = {1, 2, 3};
+        int[] s2 = {1, 2, 3};
         Arrays.equals(s1, s2);
 
         printText("_".repeat(20));
+
+        //2D Arrays
+        int[][] array = {{1, 2, 3}, {4, 5, 6}};
+        for(var outer: array){
+            for(var element: outer){
+                System.out.println(element);
+            }
+        }
+
+        System.out.println(Arrays.deepToString(array));
     }
 }
