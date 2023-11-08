a6b9dbcf9df319d19f6f3c6ba7d37794d5554292
Harsha Murupudi <itsmeeeharsha@gmail.com>
2023-11-08 15:59:47 -0600

    ADD: If else

---
+++ src/IfElse.java
@@ -0,0 +1,14 @@
+public class IfElse {
+
+    public static void main(String[] args) {
+        String test = "test";
+
+        if (test == "test") {
+            System.out.println("test");
+        } else if (test == "Test") {
+            System.out.println("Test");
+        } else {
+            System.out.println("No test");
+        }
+    }
+}
