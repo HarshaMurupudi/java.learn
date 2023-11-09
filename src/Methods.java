9bd973b3b648817374d3441971ec768bfbcd7cf0
Harsha Murupudi <itsmeeeharsha@gmail.com>
2023-11-09 16:01:04 -0600

    ADD: Methods

---
+++ src/Methods.java
@@ -0,0 +1,12 @@
+public class Methods {
+
+    //structure
+    public static float methodName(int one, float two) {
+        return one + two;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
