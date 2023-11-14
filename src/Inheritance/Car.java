b03c38b94c5dce6bc27cba7b48c72ad6fa65e3e8
Harsha Murupudi <itsmeeeharsha@gmail.com>
2023-11-14 16:10:13 -0600

    ADD: Getters & setters

--- src/Inheritance/Car.java
+++ src/Inheritance/Car.java
@@ -19,6 +19,46 @@ public class Car {
     private int doors;
     private boolean convertible;
 
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getMake(){
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getDoors() {
+        return doors;
+    }
+
+    public void setDoors(int doors) {
+        this.doors = doors;
+    }
+
+    public boolean isConvertible() {
+        return convertible;
+    }
+
+    public void setConvertible(boolean convertible) {
+        this.convertible = convertible;
+    }
+
     public void describeCar() {
         System.out.println(doors + color +  make+  model + convertible);
     }
--- src/PrimitiveTypes.java
+++ src/PrimitiveTypes.java
@@ -2,12 +2,14 @@ public class PrimitiveTypes {
     public static void main(String[] args) {
 
         //whole number
+            //default 0
         long maxLong = Long.MAX_VALUE;
         byte maxByte = Byte.MAX_VALUE; //smallest range
         int maxInt =  Integer.MAX_VALUE; //2147483647 //default
         short maxShort = Short.MAX_VALUE;
 
         //real number
+            //default 0.0
         float maxFloat = Float.MAX_VALUE;
         double maxDouble = Double.MAX_VALUE;
 
@@ -15,6 +17,7 @@ public class PrimitiveTypes {
         char character = Character.MAX_VALUE;
 
         //boolean
+            //default false
         boolean bool = Boolean.FALSE;
 
         System.out.println(maxInt + " "
