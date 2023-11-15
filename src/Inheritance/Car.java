4269865530d9a12097ea158718ce01e606e505b6
Harsha Murupudi <itsmeeeharsha@gmail.com>
2023-11-15 13:18:55 -0600

    ADD: Static variable & methods

--- src/Inheritance/Car.java
+++ src/Inheritance/Car.java
@@ -2,15 +2,15 @@ package Inheritance;
 
 
 //Access modifiers for the class
-    //public: Any other class in any package can access this class
-    //      : When omitted. Package private access
+//public: Any other class in any package can access this class
+//      : When omitted. Package private access
 
 //Access modifiers for class members
-    //public: any other class in package can access this class
-    //protected: allows classes in the same package & subclasses in other packages to have access to the member
-    //        : omitted. package access. only classes in the same package can access
-    //private: no other class can access this member
-        //(go always with this) (encapsulation)
+//public: any other class in package can access this class
+//protected: allows classes in the same package & subclasses in other packages to have access to the member
+//        : omitted. package access. only classes in the same package can access
+//private: no other class can access this member
+//(go always with this) (encapsulation)
 public class Car {
 
     private String make;
@@ -19,11 +19,43 @@ public class Car {
     private int doors;
     private boolean convertible;
 
+    //static variable
+    private static String name;
+
+    //static method
+        // cant use this
+    public static void printSumOfWheels(int a, int b){
+        System.out.println("SUM= " + (a + b));
+    }
+
+
+    //constructor
+    public Car() {
+        //constructor chaining //must first line
+        this("Kia", "Celtos");
+        System.out.println("Car constructor");
+    }
+
+    public Car(String name){
+        Car.name = name;
+    }
+
+    //constructor overloading
+    public Car(String make, String model) {
+        this.make = make;
+        this.model = model;
+    }
+
+    public void printName(){
+        System.out.println("name = " + name);
+    }
+
+
     public void setMake(String make) {
         this.make = make;
     }
 
-    public String getMake(){
+    public String getMake() {
         return make;
     }
 
@@ -60,6 +92,6 @@ public class Car {
     }
 
     public void describeCar() {
-        System.out.println(doors + color +  make+  model + convertible);
+        System.out.println(doors + color + make + model + convertible);
     }
 }
--- src/Inheritance/Main.java
+++ src/Inheritance/Main.java
@@ -3,8 +3,22 @@ package Inheritance;
 public class Main {
     public static void main(String[] args){
 
+        //constructor
+            //default constructor //no parameters
         Car car = new Car();
         car.setMake("Hyundai");
         car.describeCar();
+
+        //constructor
+        Car paramCar = new Car("Honda", "Accord");
+
+        Car oneCar = new Car("one");
+        Car secondCar = new Car("second");
+        oneCar.printName(); // second
+        secondCar.printName(); //second
+
+        //static methods
+        Car.printSumOfWheels(2, 4);
+
     }
 }
