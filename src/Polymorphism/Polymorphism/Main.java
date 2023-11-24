5b1bfac127580c23b73c5e887fd7afd34a473a2e
Harsha Murupudi <itsmeeeharsha@gmail.com>
2023-11-24 10:59:13 -0600

    ADD: Polymorphism classes

--- src/Polymorphism/Polymorphism/Main.java
+++ src/Polymorphism/Polymorphism/Main.java
@@ -1,4 +1,11 @@
 package Polymorphism.Polymorphism;
 
 public class Main {
+    public static void main(String[] args){
+        Movie movie = new Movie("Star Wars");
+        movie.watchMovie();
+
+        Movie theMovie = new Adventure("Star Wars");
+        theMovie.watchMovie();
+    }
 }
---
+++ src/Polymorphism/Polymorphism/Movie.java
@@ -0,0 +1,52 @@
+package Polymorphism.Polymorphism;
+
+public class Movie {
+    private String title;
+
+    public Movie(String title) {
+        this.title = title;
+    }
+
+    public void watchMovie(){
+        String instanceType = this.getClass().getSimpleName();
+        System.out.println(title + " is a " + instanceType + " film");
+    }
+}
+
+class Adventure extends Movie{
+    public Adventure(String title) {
+        super(title);
+    }
+
+    @Override
+    public void watchMovie() {
+        super.watchMovie();
+        System.out.printf(".. %s%n".repeat(3), "Pleaseant Scene", "Scary Music", "Something bad");
+    }
+}
+
+class Comedy extends Movie{
+    public Comedy(String title) {
+        super(title);
+    }
+
+    @Override
+    public void watchMovie() {
+        super.watchMovie();
+        System.out.printf(".. %s%n".repeat(3), "Funny Scene", "Funny Music", "Happy ending");
+    }
+}
+
+class SciFi extends Movie{
+    public SciFi(String title) {
+        super(title);
+    }
+
+    @Override
+    public void watchMovie() {
+        super.watchMovie();
+        System.out.printf(".. %s%n".repeat(3), "Alien Scene", "Alien Music", "Shows the alien");
+    }
+}
+
+
