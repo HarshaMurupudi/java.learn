bf913cc32df0bc174204ddb6fd30e586ea4efdef
Harsha Murupudi <itsmeeeharsha@gmail.com>
2023-11-29 12:51:15 -0600

    ADD: Packages example

---
+++ src/Packages/src/dev/harsha/Main.java
@@ -0,0 +1,14 @@
+package Packages.src.dev.harsha;
+
+import Packages.src.com.abc.first.Item;
+//import Packages.src.com.abc.first.*
+public class Main {
+
+    public static void main(String[] args){
+        Item firstItem = new Item("burger");
+        //Packages.src.com.abc.first.Item firstItem = new Item("Burger");
+
+        System.out.println(firstItem);
+
+    }
+}
