3d7fa28853f698ffd79d547ef42ce3f02e59e495
Harsha Murupudi <itsmeeeharsha@gmail.com>
2023-11-30 16:07:32 -0600

    ADD: List documentation

---
+++ src/Lists/JList.java
@@ -0,0 +1,21 @@
+package Lists;
+
+//resize feature
+
+//List Interface
+//List<E> extends Collections<E> externs Iterable<T>
+//Implementing classes
+    // ArrayList<E> -> AbstractList<E> -> List<E>
+        // Stack<E> -> Vector<E> -> AbstractList<E>
+    // LinkedList<E> -> List<E>
+    // CopyOnWriteArrayList<E> -> List<E>
+public class JList {
+
+    //int size();
+    //boolean isEmpty;
+    //boolean contains(Object o);
+    //Iterator<E> iterator();
+    //Object[] toArray();
+    //
+
+}
