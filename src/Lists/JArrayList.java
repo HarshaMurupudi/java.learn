e6292e2925c7970bb0b48e3a4f19695d86a83362
Harsha Murupudi <itsmeeeharsha@gmail.com>
2023-12-02 10:08:54 -0600

    ADD: Add more methods to ArrayList

--- src/Lists/JArrayList.java
+++ src/Lists/JArrayList.java
@@ -1,4 +1,51 @@
 package Lists;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+record GroceryItem(String name, String type, int count) {
+    public GroceryItem(String name) {
+        this(name, "Dairy", 1);
+    }
+}
+
 public class JArrayList {
+
+    public static void main(String[] args) {
+        String[] vowels = {"a", "b", "c"};
+
+        ArrayList<GroceryItem> groceryItems = new ArrayList<>();
+        groceryItems.add(new GroceryItem("Butter"));
+
+
+        List<String> items = List.of(vowels);
+        ArrayList<String> aList = new ArrayList<>(items);
+        aList.add("D");
+        ArrayList<String> aCompactList = new ArrayList<>(List.of("pickels", "mustard"));
+        aCompactList.add("D");
+        aList.addAll(aCompactList);
+        System.out.println(aList);
+        aList.set(0, "Z");
+
+        aList.get(2);
+        aList.contains("D");
+        aList.indexOf("D");
+        aList.lastIndexOf("D");
+        aList.size();
+
+        groceryItems.remove(0);
+        aList.remove(1);
+        aList.remove("D");
+        aList.removeAll(List.of("D", "a"));
+        aList.retainAll(List.of("a", "b")); //only saves these
+        aList.clear();
+
+        aList.isEmpty();
+        aList.contains("D");
+        aList.containsAll(List.of("a"));
+
+        aList.sort(Comparator.naturalOrder());
+        aList.sort(Comparator.reverseOrder());
+    }
 }
--- src/Lists/JList.java
+++ src/Lists/JList.java
@@ -2,13 +2,18 @@ package Lists;
 
 //resize feature
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
 //List Interface
 //List<E> extends Collections<E> externs Iterable<T>
 //Implementing classes
-    // ArrayList<E> -> AbstractList<E> -> List<E>
-        // Stack<E> -> Vector<E> -> AbstractList<E>
-    // LinkedList<E> -> List<E>
-    // CopyOnWriteArrayList<E> -> List<E>
+// ArrayList<E> -> AbstractList<E> -> List<E>
+// Stack<E> -> Vector<E> -> AbstractList<E>
+// LinkedList<E> -> List<E>
+// CopyOnWriteArrayList<E> -> List<E>
 public class JList {
 
     //int size();
@@ -16,6 +21,39 @@ public class JList {
     //boolean contains(Object o);
     //Iterator<E> iterator();
     //Object[] toArray();
-    //
+    //boolean add(E e)
+    //boolean remove(Object o)
+    //boolean retainAll(Collection<?> c)
+    //void clear(): Removes all the elements form the list
+    //E get(int index)
+    //E set(int index, E element)
+    //ListIterator<E> listIterator() //returns a list iterator over the elements
+    //List<E> subList(int fromIndex, int toIndex)
 
+    // default void replaceAll(UnaryOperator<E> operator) //replaces each element with the result of applying the operator
+    //default void sort(Comparator<super E> c)
+    //default Spliterator<E> spliterator()
+
+    //Array to List
+    public static void main(String[] args) {
+        String[] vowels = {"a", "b", "c"};
+
+        //Backed by array so can't do structural mod
+        List<String> vowelList = Arrays.asList(vowels);
+        System.out.println(vowelList);
+        System.out.println(vowels);
+
+        //can be modified
+        List<String> myList = new ArrayList<>();
+        for (String s : vowels) {
+            myList.add(s);
+        }
+        System.out.println(myList);
+
+        List<String> items = List.of(vowels);
+        ArrayList<String> aList = new ArrayList<>(items);
+        aList.add("D");
+
+        var gArray = aList.toArray(new String[aList.size()]);
+    }
 }
